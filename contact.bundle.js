07e443f82bcfd569a07d645dc577846768c50202
Dan Beck <beckdp01@gmail.com>
2022-10-14 12:55:30 -0400

    complete basic tab switching functionality

---
+++ contact.bundle.js
@@ -0,0 +1,37 @@
+"use strict";
+(self["webpackChunkrestaurant_page_v2"] = self["webpackChunkrestaurant_page_v2"] || []).push([["contact"],{
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ loadContact)
+/* harmony export */ });
+function loadContact() {
+    const content = document.getElementById('content');
+    const header = document.createElement('h1');
+    const headerNode = document.createTextNode("Contact Us");
+    header.appendChild(headerNode);
+
+    const text = document.createElement('p');
+    const copyNode = document.createTextNode('Lorem Ipsum. Sic Transit Gloria.');
+    text.appendChild(copyNode);
+
+    content.appendChild(header);
+    content.appendChild(text);
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/contact.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udGFjdC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFlO0FBQ2Y7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnRfcGFnZV92Mi8uL3NyYy9jb250YWN0LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIGxvYWRDb250YWN0KCkge1xuICAgIGNvbnN0IGNvbnRlbnQgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnY29udGVudCcpO1xuICAgIGNvbnN0IGhlYWRlciA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2gxJyk7XG4gICAgY29uc3QgaGVhZGVyTm9kZSA9IGRvY3VtZW50LmNyZWF0ZVRleHROb2RlKFwiQ29udGFjdCBVc1wiKTtcbiAgICBoZWFkZXIuYXBwZW5kQ2hpbGQoaGVhZGVyTm9kZSk7XG5cbiAgICBjb25zdCB0ZXh0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgncCcpO1xuICAgIGNvbnN0IGNvcHlOb2RlID0gZG9jdW1lbnQuY3JlYXRlVGV4dE5vZGUoJ0xvcmVtIElwc3VtLiBTaWMgVHJhbnNpdCBHbG9yaWEuJyk7XG4gICAgdGV4dC5hcHBlbmRDaGlsZChjb3B5Tm9kZSk7XG5cbiAgICBjb250ZW50LmFwcGVuZENoaWxkKGhlYWRlcik7XG4gICAgY29udGVudC5hcHBlbmRDaGlsZCh0ZXh0KTtcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
