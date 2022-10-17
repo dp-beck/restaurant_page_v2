b773c78f047ca4aca036203b349a6c128ee09d62
Dan Beck <beckdp01@gmail.com>
2022-10-16 21:01:12 -0400

    add content and style contact us page

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,5 @@
+import restaurant from "./restaurant.jpg";
+
 export default function loadContact() {
     const content = document.getElementById('content');
     const header = document.createElement('h1');
@@ -5,9 +7,16 @@ export default function loadContact() {
     header.appendChild(headerNode);
 
     const text = document.createElement('p');
-    const copyNode = document.createTextNode('Lorem Ipsum. Sic Transit Gloria.');
+    const copyNode = document.createTextNode('123 Main Street \
+                                            Anytown, KY 25846 \
+                                            258-658-7875');
     text.appendChild(copyNode);
+    text.setAttribute("id", "address");
+
+    const image = document.createElement('img');
+    image.setAttribute("src", restaurant);
 
     content.appendChild(header);
     content.appendChild(text);
+    content.appendChild(image);
 }
